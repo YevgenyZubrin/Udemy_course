52ff3fabf670a6d7fb85bfbf3c90d59ddc97a658
Eugen <yevgeniy.zubrin@gmail.com>
2021-03-01 11:54:17 +0600

    first commit

---
+++ js/script1.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// let number = 10.5;
+
+// const person = 'Mike';
+
+// let bool = true;
+
+
+
+// console.log(number);
+// console.log(person);
+// console.log(bool);
+// console.log(something);
+ 
+// let und;
+// console.log(und);
+
+// let obj = {
+//     name: 'Alex',
+//     age: 28,
+//     isMarried: true
+// };
+
+// console.log(obj["isMarried"]);
+
+// let arr = [1, 5, 10, 'three', true, {}, []];
+// console.log(arr[3]);
---
+++ js/script2.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// alert('hello');
+
+// const result = confirm('Are you here?');
+
+// const answer = +prompt('вам есть 18?', '18');
+// console.log(answer + 10);
+// console.log(typeof(answer));
+
+const answers = [];
+
+// answers[0] = prompt('как ваше имя?', '');
+// answers[1] = prompt('как вашa фамилия?', '');
+// answers[2] = prompt('сколько вам лет?', '');
+
+// document.write(answers);
+
+console.log(typeof(answers));
