229e6214fde29ea6f66c2294cf9d39674dac2a49
Eugen <yevgeniy.zubrin@gmail.com>
2021-03-02 10:45:04 +0600

    practiced with methods

---
+++ js/script9.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const str = 'test';
+// const arr = [1, 2, 3];
+
+console.log(str.toUpperCase());
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("w"));
+
+
+const logg = "Hello world!";
+
+// console.log(logg.slice(6, 11));
+
+// console.log(logg.substring(6, 11));
+
+// console.log(logg.substr(6, 5));
+
+const num = 12.5;
+
+console.log(Math.round(num));
+
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
