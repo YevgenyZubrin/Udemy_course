5d81ef5c2e9e5735efc2aae5700c0464696c027b
Eugen <yevgeniy.zubrin@gmail.com>
2021-03-03 14:57:37 +0600

    forgot to save script15.js file. saved

--- js/script15.js
+++ js/script15.js
@@ -17,12 +17,16 @@ const soldier = {
     },
 };
 
-const john = {
-    health: 100,
-};
+const john = Object.create(soldier);
+
+
+// const john = {
+//     health: 100,
+// };
 
 // john.__proto__ = soldier;
-Object.setPrototypeOf(john, soldier);
+
+// Object.setPrototypeOf(john, soldier);
 
 console.log(john);
 console.log(john.armor);
